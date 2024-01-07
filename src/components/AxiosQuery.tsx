6a612765f94c52e2d589fb41d938b190f56c36e9
김다빈 <991012dabin@gmail.com>
2024-01-07 22:59:59 +0900

    feat: axios로 비동기 데이터 불러오기

--- src/components/AxiosQuery.tsx
+++ src/components/AxiosQuery.tsx
@@ -1,13 +1,46 @@
 import { useEffect, useState } from "react";
+import instance from "../api/instance";
+import { isAxiosError } from "axios";
 
+interface ResponseDataType {
+  message: string;
+  code: number;
+}
 const AxiosQuery = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const [error, setError] = useState("");
 
-  useEffect(() => {}, []);
+  const fetchUser = async () => {
+    try {
+      const { data } = await instance.get("users");
+      setData(data);
+    } catch (error) {
+      if (isAxiosError<ResponseDataType>(error)) {
+        setError(error.message);
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-  return <div>AxiosQuery</div>;
+  useEffect(() => {
+    setIsLoading(true);
+    fetchUser();
+  }, []);
+
+  if (isLoading) return <>Loading...</>;
+
+  if (error) return <h2>{error}</h2>;
+
+  return (
+    <>
+      <div className="text-4xl">AxiosQuery</div>
+      <ul className="list-disc p-4">
+        {data && data.map((user) => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
+  );
 };
 
 export default AxiosQuery;
