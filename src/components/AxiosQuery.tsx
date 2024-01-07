c0a170093978f9cf9fab3bffe0e5cfc18d73f833
김다빈 <991012dabin@gmail.com>
2024-01-07 22:37:38 +0900

    feat: axios instance 생성

--- src/components/AxiosQuery.tsx
+++ src/components/AxiosQuery.tsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from "react";
+
 const AxiosQuery = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {}, []);
+
   return <div>AxiosQuery</div>;
 };
 
